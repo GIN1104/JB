767594b74526ee83c5efee4b4b42d8e5f79f3756
Evgeny <starchenkogin@gmail.com>
2020-06-24 21:45:01 +0300

    first commit

---
+++ 2020-06-23/script.js
@@ -0,0 +1,33 @@
+
+let albums = [],
+    url = "https://jsonplaceholder.typicode.com/albums",
+    selectedAlbum = [];
+
+let getAlbums = () => {
+    $.ajax({
+        type: 'GET',
+        datatype: 'json',
+        url: url,
+        async: false,
+        success: function (data) {   
+            albums = data;
+            addRateToAlbum();
+        },
+        error: function (error) {
+            console.log("error : ", error);
+        }
+    });
+}
+
+getAlbums();
+
+let addRateToAlbum =() => {
+albums.map( album =>{
+    album.rate = Math.floor(Math.random() * 100) + 1;
+    if(album.rate > 80)
+       selectedAlbum.push(album);
+     return album
+    })
+}
+// albums.range = Math.floor(Math.random() * 100) + 1;
+ //console.log("Albums : ", albums);
